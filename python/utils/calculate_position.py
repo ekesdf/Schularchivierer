7a032e9b6740ecfa109af2bd0209fd22196d8f6d
ekesdf <petzoldpascal@gmx.de>
2021-06-18 20:41:41 +0200

    fixed rounding issue

--- python/utils/calculate_position.py
+++ python/utils/calculate_position.py
@@ -10,7 +10,17 @@ def calculate_position_of_char_in_the_pdf(image_shape,pdf_shape,coordinates):
     x = coordinates[0]
     y = coordinates[1]
 
-    corrected_x = round(interp(x,[0,image_width],[0,pdf_width]))
-    corrected_y = round(interp(y,[0,image_height],[0,pdf_height]))
+#    corrected_x = round(interp(x,[0,image_width],[0,pdf_width]))
+#    corrected_y = round(interp(y,[0,image_height],[0,pdf_height]))
+
+
+    if x % 1 > 0.5: corrected_x = round(x)-1
+
+    else: corrected_x = round(x)
+
+    if y % 1 > 0.5: corrected_y =round(y)-1
+
+    else: corrected_y =round(y)
+
 
     return corrected_x, corrected_y
