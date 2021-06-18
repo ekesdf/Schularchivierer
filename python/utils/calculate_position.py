85162b0e63eadbf2bbb8903cd4de71e07afff78d
ekesdf <petzoldpascal@gmx.de>
2021-06-18 20:44:13 +0200

    cleaned up the code by deleting unessecarry vars

--- python/utils/calculate_position.py
+++ python/utils/calculate_position.py
@@ -3,16 +3,8 @@ from numpy import interp
 
 def calculate_position_of_char_in_the_pdf(image_shape,pdf_shape,coordinates):
 
-    image_width = image_shape[0]
-    image_height = image_shape[1]
-    pdf_width = pdf_shape[0]
-    pdf_height = pdf_shape[1]
-    x = coordinates[0]
-    y = coordinates[1]
-
-#    corrected_x = round(interp(x,[0,image_width],[0,pdf_width]))
-#    corrected_y = round(interp(y,[0,image_height],[0,pdf_height]))
-
+    x = round(interp(coordinates[0],[0,image_shape[0]],[0,pdf_shape[0]]))
+    y = round(interp(coordinates[1],[0,image_shape[1]],[0,pdf_shape[1]]))
 
     if x % 1 > 0.5: corrected_x = round(x)-1
 
